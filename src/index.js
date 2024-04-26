00051dd9bf4fa9e8327faa57a4bc8411c90c98e0
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-04-26 17:18:27 +0200

    gallery-done

--- src/index.js
+++ src/index.js
@@ -1,53 +1,28 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
-import { getImages } from './services';
+import { getImages, renderImages } from './services';
 
 const form = document.querySelector('form#search-form');
-const gallery = document.querySelector('div.gallery');
+const moreBtn = document.querySelector('button.load-more');
+let page = 1;
 
-form.addEventListener('submit', ev => {
+form.addEventListener('submit', async ev => {
   ev.preventDefault();
-  let query = ev.target.elements.searchQuery.value;
+  let query = ev.currentTarget.elements.searchQuery.value;
   console.log(query);
-  const searchParams = new URLSearchParams({
-    q: `${query}`,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-  getImages(searchParams)
-    .then(images => {
-      console.log(images.hits);
-      const cardList = images.hits
-        .map(image => {
-          return `<div class="photo-card">
-          <img src="${image.webFormatURL}" alt="${tags}" loading="lazy" />
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
-              ${image.likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>
-              ${image.views}
-            </p>
-            <p class="info-item">
-              <b>Comments</b>
-              ${image.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>
-              ${image.downloads}
-            </p>
-          </div>
-        </div>`;
-        })
-        .join('');
-      gallery.innerHTML = cardList;
-    })
-    .catch(error => {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    });
+
+  try {
+    const response = await getImages(query);
+    Notify.success(`Hooray WE found ${response.totalHits} images!`);
+    const images = response.hits;
+    renderImages(images);
+    moreBtn.classList.remove('hidden');
+    page += 1;
+    if (page > 1) {
+    }
+  } catch (error) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
 });
--- src/services.js
+++ src/services.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const apiKey = '43560822-853d258fb61c3d5dd4d985685';
+const gallery = document.querySelector('div.gallery');
 
 // export const getImages = async searchParams => {
 //   const response = await axios.get(
@@ -8,10 +9,46 @@ const apiKey = '43560822-853d258fb61c3d5dd4d985685';
 //   return response.data;
 // };
 
-export async function getImages(searchParams) {
-  return await axios
-    .get(`https://pixabay.com/api/?key=${apiKey}&${searchParams}`)
-    .then(response => {
-      return response.data;
-    });
+export async function getImages(query, page) {
+  const searchParams = new URLSearchParams({
+    q: `${query}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: page,
+    per_page: 40,
+  });
+  const response = await axios.get(
+    `https://pixabay.com/api/?key=${apiKey}&${searchParams}`
+  );
+  return response.data;
+}
+export function renderImages(images) {
+  console.log(images);
+  const cardList = images
+    .map(image => {
+      return `<div class="photo-card">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              ${image.likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              ${image.views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              ${image.comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              ${image.downloads}
+            </p>
+          </div>
+        </div>`;
+    })
+    .join('');
+  gallery.innerHTML = cardList;
 }
